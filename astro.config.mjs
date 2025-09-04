891c0cf73c1fa0672c4fd548d4b4dec657e8b84b
haibing3344 <haibing3344@gmail.com>
2025-09-04 21:27:00 +0800

    add clashforwin

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,6 +48,17 @@ export default defineConfig({
 					label: '常见问题',
 					slug: 'faq',
 				},
+				{
+					label: '⭐️科学上网梯子加速器机场推荐',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: '🚀优信云', badge: { text: 'IEPL/IPLC 高速专线', variant: 'success' },link: 'https://a.suola.link/youxinyun' },
+						{ label: '🚀扬帆云', badge: { text: '老牌机场分站', variant: 'note' },link: 'https://a.suola.link/yfy' },
+						{ label: '🚀疾风云', badge: { text: '可做备用机场', variant: 'tip' },link: 'https://a.suola.link/jifeng' },
+						{ label: '🚀尔湾云', badge: { text: '性价比高', variant: 'danger' },link: 'https://a.suola.link/erwan' },
+						{ label: '🚀网际快车', badge: { text: '性价比高', variant: 'caution' },link: 'https://a.suola.link/wjkc' },
+					],
+				},
 			],
 		}),
 	],
