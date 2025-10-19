94598faec55c19265304d922025955bdf245df61
haibing3344 <haibing3344@gmail.com>
2025-10-19 08:30:04 +0800

    add nofollow

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
 				{
 					label: '🎉机场推荐',
 					badge: '推荐',
-					slug: 'feed',
+					link: 'https://jichangtuijian.uk',
 				},
 				{
 					label: '介绍',
@@ -52,26 +52,14 @@ export default defineConfig({
 					label: '⭐️科学上网梯子加速器机场推荐',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: '🚀优信云', badge: { text: 'IEPL/IPLC 高速专线', variant: 'success' },link: 'https://a.suola.link/youxinyun' },
-						{ label: '🚀扬帆云', badge: { text: '老牌机场分站', variant: 'note' },link: 'https://a.suola.link/yfy' },
+						{ label: '🚀扬帆云', badge: { text: 'IEPL/IPLC 高速专线', variant: 'success' },link: 'https://a.suola.link/yfy' },
+						{ label: '🚀优信云', badge: { text: '老牌机场分站', variant: 'note' },link: 'https://a.suola.link/youxinyun' },
 						{ label: '🚀疾风云', badge: { text: '可做备用机场', variant: 'tip' },link: 'https://a.suola.link/jifeng' },
 						{ label: '🚀尔湾云', badge: { text: '性价比高', variant: 'danger' },link: 'https://a.suola.link/erwan' },
 						{ label: '🚀网际快车', badge: { text: '性价比高', variant: 'caution' },link: 'https://a.suola.link/wjkc' },
 					],
 				},
-				{
-					label: '其它代理客户端',
-					items: [
-						{ label: ' Shadowrocket(小火箭)', badge: { text: 'iOS专业网络工具', variant: 'success' }, link: 'https://shadowrocket.uk' },
-						{ label: ' ClashMetaForAndroid', badge: { text: 'Android专业网络工具', variant: 'note' }, link: 'https://clashmeta.bid' },
-						{ label: ' ClashForWindows', badge: { text: 'Windows平台代理工具', variant: 'tip' }, link: 'https://clashverge.uk' },
-						{ label: ' ClashMi', badge: { text: '基于ClashMeta的跨平台代理客户端', variant: 'danger' }, link: 'https://clashmi.uk' },
-						{ label: ' Flclash', badge: { text: '基于ClashMeta的多平台代理客户端', variant: 'caution' }, link: 'https://flclash.men' },
-						{ label: ' Karing', badge: { text: '新一代网络代理工具,免费', variant: 'tip' }, link: 'https://karings.org' },
-						{ label: ' Stash', badge: { text: '简单且强大的网络工具', variant: 'success' }, link: 'https://stashapp.uk' },
-						{ label: ' Loon', badge: { text: 'iOS平台最强大的网络代理工具', variant: 'tip' }, link: 'https://nsloon.uk' },
-					],
-				},
+
 			],
 		}),
 	],
--- src/content/docs/wiki/VMess.md
+++ src/content/docs/wiki/VMess.md
@@ -20,16 +20,16 @@ VMess是V2Ray项目中使用的一种加密传输协议，用于实现客户端�
 Clash for Windows 是一个图形用户界面（GUI）客户端，能够支持和管理多种网络代理协议，包括VMess。通过Clash for Windows，用户可以方便地配置和使用VMess节点，并结合其他规则和代理类型进行灵活的流量管理。
 
 :::tip[🎉 节点推荐]
-- 🚀 [Cyberguard: 解锁Netflix/Hbo/Disney+/Dazn等流媒体,18.00元/月](https://www.cyberguard.best/#/register?code=XsreC0T5)<br>
-- 🚀 [优信云：IEPL/IPLC 高速专线，￥15.00/月](https://www.优信云.com/#/register?code=JRtE5uIV)<br>
-- 🚀 [尔湾云：最大峰值1000Mbps，全流媒体及ChatGPT解锁！最低12元/月](https://erwan6.net/auth/register?code=BoObCd)<br>
-- 🚀 [TNTCloud：新开机场，季付30，每月低至￥10.00/月](https://haibing822.tntvipaff.cc/#/register?code=GtjJVgml)<br>
-- 🚀 [魔戒：不限时，不限制使用人数，直至套餐流量用完，低至￥14.9/130G流量](https://mojie.app/#/register?code=sSdtPtLo)<br>
+- 🚀 [Cyberguard: 解锁Netflix/Hbo/Disney+/Dazn等流媒体,18.00元/月](https://a.suola.link/cyberguard)<br>
+- 🚀 [优信云：IEPL/IPLC 高速专线，￥15.00/月](https://a.suola.link/youxinyun)<br>
+- 🚀 [尔湾云：最大峰值1000Mbps，全流媒体及ChatGPT解锁！最低12元/月](https://a.suola.link/erwan)<br>
+- 🚀 [TNTCloud：新开机场，季付30，每月低至￥10.00/月](https://a.suola.link/tnt)<br>
+- 🚀 [魔戒：不限时，不限制使用人数，直至套餐流量用完，低至￥14.9/130G流量](https://a.suola.link/mojie)<br>
 - 🚀 [宝可梦星云：新用户首单：9折优惠码：9999，低至5.9/月 ](https://a.suola.link/pokemon)<br>
-- 🚀 [NanoCloud: 绑定TG机器人每天领取免费流量，月付最低1元/月](https://edu.uodoo.bid/auth/register?code=JMiOQDHf)<br>
-- 🚀 [肥猫云：全专线中转隧道，高速大宽带不限设备数，低至6元/月（年付）](https://fchb1188.fcvipaff.cc/register?aff=X1vZd2wf)<br>
-- 🚀 [疾风云：包年 6.9折 优惠码: jf2025](https://homes.tr25.cn?code=ReCm)<br>
-- 🚀 [闪狐云：不限速，不限设备。高速专线。20元/月](https://inv02.ffaff.cc/register?aff=WQApz2pv)
+- 🚀 [NanoCloud: 绑定TG机器人每天领取免费流量，月付最低1元/月](https://a.suola.link/nanocloud)<br>
+- 🚀 [肥猫云：全专线中转隧道，高速大宽带不限设备数，低至6元/月（年付）](https://a.suola.link/feimao)<br>
+- 🚀 [疾风云：包年 6.9折 优惠码: jf2025](https://a.suola.link/jifeng)<br>
+- 🚀 [闪狐云：不限速，不限设备。高速专线。20元/月](https://a.suola.link/shy)
 :::
 
 :::note[💬 交流群]
